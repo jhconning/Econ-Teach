a357be1446e523ead5b0d04edc8282e129c02b4c
Jonathan Conning <jhconning@users.noreply.github.com>
2019-09-18 17:13:06 -0400

    solve for world eqn prices

--- econteach.py
+++ econteach.py
@@ -7,6 +7,7 @@ Created on Mon Sep 16 08:57:01 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
 
 
 plt.style.use('bmh')
@@ -18,15 +19,15 @@ plt.rcParams["font.size"] = 18
 
 
 # Some default parameters when none are supplied
-MPLX = 1
+MPLX = 2
 MPLY = 1
 LBAR = 100
 
 NAMEX = 'Good X'
 NAMEY = 'Good Y'
 
-XMAX = LBAR*MPLX*4   # for plot bounds and aesthetics
-YMAX = LBAR*MPLY*4
+XMAX = 400   # for plot bounds and aesthetics
+YMAX = 400
 N = 50              # num of datapoints to plot
 QX =  np.linspace(0.1, XMAX, N)
 
@@ -38,9 +39,9 @@ def budget(px=1, py=1, I=100, show = False):
     plt.plot(QX, qy, linewidth=2, label='Budget')
     plt.axis([0,XMAX,0,YMAX])
     plt.xlabel(NAMEX), plt.ylabel(NAMEY)
-    plt.text(0.3*XMAX, 0.9*YMAX, 
-             r'   $\frac{P_X}{P_Y}=$'+'{:3.2f}'.format(px/py)+' Y/X',
-             fontsize=12)
+    #plt.text(0.3*XMAX, 0.9*YMAX, 
+    #         r'   $\frac{P_X}{P_Y}=$'+'{:3.2f}'.format(px/py)+' Y/X',
+    #         fontsize=12)
     if show: #use False for subplots
         plt.show();
         
@@ -50,41 +51,91 @@ def ppf(mplx=MPLX, mply=MPLY, lbar=LBAR, show = False, title='Home'):
     qy = mply*lbar - (mply/mplx) * QX
     plt.plot(QX, qy, linewidth=2, label='PPF')
     plt.axis([0,XMAX,0,YMAX])
-    plt.xlabel(NAMEX), plt.ylabel(NAMEY), plt.title(title)
+    plt.xlabel(NAMEX), plt.ylabel(NAMEY)
+    plt.title(title)
     plt.text(0.15*XMAX, 0.9*YMAX, 
              r' $\frac{MPL_Y}{MPL_X}=\frac{P_X}{P_Y}=$'+'{:3.2f}'.format(mply/mplx)
              + ' Y/X', fontsize=14  )  
     if show: #use False for subplots
         plt.show();
         
-def openppf(mplx, mply, lbar, pw, show=True, title='Home'):
-    ppf(mplx, mply, lbar, show=False, title=title)
-    if pw < (mply/mplx):
-        plt.plot(QX, mply*lbar - pw * QX, linestyle='--' )
-        plt.scatter(0, mply*lbar, s=100, clip_on=False)
-    else:
-        plt.plot(QX, pw*mplx*lbar - pw * QX, linestyle='--' )
-        plt.scatter(mplx*lbar,0, s=100, clip_on=False)
-  
-def twopane(mplx, mply, lbar, mplfx, mplfy, lbarf, p):
-    foreign = openppf
+def twoppf(mplx, mply, lbar, mplfx, mplfy, lbarf):
+    foreign = ppf
     plt.figure(1, figsize =(12,12))
     plt.subplot(121, aspect='equal')
-    openppf(mplx, mply, lbar, p, show = False)
+    ppf(mplx, mply, lbar, title='Home')
     plt.subplot(122, aspect='equal')
-    foreign(mplfx, mplfy, lbarf, p, show=False, title='Foreign')
-    plt.ylabel(''); 
-
+    foreign(mplfx, mplfy, lbarf, title='Foreign')
+    plt.ylabel('')
+          
+       
+# We assume very simple Cobb-Douglas preferences
+    
 def indif(x, ubar):
     return ubar/x      
 
+def demands(p, I):
+     return I/(2*p) , I/2
+
 def indif_plot(p, I):
-    xe, ye = I/(2*p) , I/2
-    plt.scatter(xe,ye)
-    ubar = xe*ye
-    QX_ = np.where( (QX>xe*0.3)&(QX<xe*2), QX, 
-                   np.nan)
+    xd, yd = demands(p, I)
+    plt.scatter(xd,yd)
+    ubar = xd*yd
+    QX_ = np.where( (QX>xd*0.3)&(QX<xd*2), QX, np.nan) #clip for aesthetics
     plt.plot(QX_, indif(QX_, ubar))
+    
+def openeq(mplx=MPLX, mply=MPLY, lbar=LBAR, pw=1):
+    if pw > mply/mplx:
+        qx = mplx*lbar
+        qy = 0
+    elif pw <= mply/mplx:
+        qx =0
+        qy = mply*lbar
+    I = pw*qx + qy       
+    cx, cy = demands(pw,I)
+    return qx, qy, cx, cy
+       
+
+def openppf(mplx=MPLX, mply=MPLY, lbar=LBAR, pw=1, show = False, title='Home'):
+    '''Like ppf but also plots world price line and specialization'''
+    ppf(mplx, mply, lbar)
+    qx, qy, cx, cy = openeq(mplx, mply, lbar, pw)
+    plt.scatter(qx,qy)
+    plt.title(title)
+    I = pw*qx + qy    #income measured in units of y
+    budget(px=pw, py=1, I=I)
+    indif_plot(pw, I)
+
+
+def worldprice(mplx, mply, lbar, mplfx, mplfy, lbarf):
+    '''  World equilibrium price consistent with balanced trade
+    By Walras' law we just need to find price at which excess demand in 
+    one market is eliminated
+    '''
+    def xsdemand(p):
+        qx, _, cx, _ = openeq(mplx, mply, lbar, p)
+        qfx, _, cfx, _ = openeq(mplfx, mplfy, lbarf, p)
+        return (cx + cfx) - (qx + qfx) 
+    
+    guess = (mply/mplx + mplfy/mplfx)/2 
+    return fsolve(xsdemand, guess)[0]
+    
+
+def twopane(mplx=MPLX, mply=MPLY, lbar=LBAR, 
+            mplfx=MPLY, mplfy=MPLX, lbarf=LBAR, p=1):
+    foreign = openppf
+    plt.figure(1, figsize =(12,12))
+    plt.subplot(121, aspect='equal')
+    openppf(mplx, mply, lbar, p, title='Home')
+    plt.subplot(122, aspect='equal')
+    foreign(mplfx, mplfy, lbarf, p, title='Foreign')
+    plt.ylabel('')
+    
+def worldeq(mplx=MPLX, mply=MPLY, lbar=LBAR, 
+            mplfx=MPLY, mplfy=MPLX, lbarf=LBAR):
+    
+    pw = worldprice(mplx, mply, lbar, mplfx, mplfy, lbarf)
+    twopane(mplx, mply, lbar, mplfx, mplfy, lbarf, p=pw)
 
     
 
